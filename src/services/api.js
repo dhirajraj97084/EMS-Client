88cd83868f8ee3360afd63a7a4b43414aa186ab7
Dhiraj  Kumar Tiwari <dhirajraj97084@gmail.com>
2025-08-15 08:58:22 +0530

    okay

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import toast from 'react-hot-toast'
 
 // Create axios instance
 export const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
